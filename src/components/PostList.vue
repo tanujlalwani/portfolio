55eea99d2a77e2adacdc493270bb2995e7cae3a2
Tanuj <tanuj.lalwani@protonmail.com>
2019-08-04 23:52:22 -0700

    [COMPONENT]: Add PostList component
    Iterates over the list of posts and displays them

---
+++ src/components/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="post-list">
+    <router-link
+      v-for="(post, index) in posts"
+      v-bind:key="index"
+      class="post-list-item"
+      :to="{
+        name: 'post',
+        params: { title: post}
+      }"
+    >
+      <img class="post-list-item-image" src="@/assets/posts/{{post}}/cover.svg" type="image/svg" />
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: ["the-aditi-wall", "reach-out-of-the-box", "binary", "artwork"]
+    };
+  }
+};
+</script>
