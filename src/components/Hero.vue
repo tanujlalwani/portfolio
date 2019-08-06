affca2a4f95a3260f8af1f269422d3718d616a24
Tanuj <tanuj.lalwani@protonmail.com>
2019-08-05 22:55:14 -0700

    [STYLE]: Add Hero layout styling

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hero">
-    <div class="hero-text"></div>
+    <div class="hero-text">Hello</div>
   </div>
 </template>
 
@@ -8,5 +8,31 @@
 export default {};
 </script>
 
-<style>
+<style lang="scss">
+.hero {
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background-color: rgb(223, 69, 22);
+
+    .hero-text {
+      width: 60%;
+      // height: 100%;
+      // flex-basis: 100%;
+
+      font: 4rem "Inter UI", system-ui, sans-serif;
+      font-weight: 650;
+      text-align: center;
+      color: #000;
+
+      letter-spacing: -0.02rem;
+      line-height: 1.1em;
+
+      cursor: default;
+    }
+  }
 </style>
