0c29efe7181dc6e0226add0d514aadf036fad6f2
Tanuj <tanuj.lalwani@protonmail.com>
2019-08-08 18:03:53 -0700

    [STYLE]: Testing out alternate Hero styles

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hero">
-    <div class="hero-text">Hello</div>
+    <div class="hero-text">Hello! I'm just testing this hero text out.</div>
   </div>
 </template>
 
@@ -10,27 +10,30 @@ export default {};
 
 <style lang="scss">
 .hero {
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    background-color: rgb(223, 69, 22);
+  background-color: #000;
 
-    .hero-text {
-      width: 60%;
+  .hero-text {
+    width: 60%;
 
-      font: 4rem "Inter UI", system-ui, sans-serif;
-      font-weight: 650;
-      text-align: center;
-      color: #000;
+    font: 3.5rem "Space Mono", system-ui, sans-serif;
+    font-weight: 800;
+    text-align: center;
+    color: rgb(255, 102, 0);
+    // -webkit-text-stroke: 1px white;
 
-      letter-spacing: -0.02rem;
-      line-height: 1.1em;
+    // letter-spacing: -0.02rem;
+    // word-spacing: -0.2rem;
+    line-height: 1.2em;
 
-      cursor: default;
-    }
+    cursor: default;
+    user-select: none;
   }
+}
 </style>
