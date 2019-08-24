30c7f81018c9e7d166109f66dd609299150646bb
Tanuj <tanuj.lalwani@protonmail.com>
2019-08-23 18:06:57 -0700

    [ADD]: Add vue-check-view script

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import checkView from 'vue-check-view';
+Vue.use(checkView);
+
 import MarqueeText from 'vue-marquee-text-component';
 Vue.component('marquee-text', MarqueeText);
 
