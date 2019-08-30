6847548a06f563b7ac6c087eadde6ad43c7a2414
Tanuj <tanuj.lalwani@protonmail.com>
2019-08-29 23:52:17 -0700

    [ADD]: Add Baffle.js for about technologist interaction

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,21 +4,27 @@
     <div class="about-text-wrapper display-flex-column">
       <p
         class="about-text"
-        v-bind:class="{ 'about-text-creative': creativeOn, 'about-text-technologist': technologistOn}"
+        v-bind:class="{ 'about-creative': creativeOn, 'about-creative-done': creativeDone, 'about-technologist': technologistOn}"
       >
-        {{ technologistOn ? technologistBinary[0] : technologistText[0] }}
         <span
-          class="creative"
+          class="about-technologist-text"
+        >{{ technologistOn ? technologistBinary[0] : technologistText[0] }}</span>
+        <span
+          class="creative about-technologist-text"
           @mouseenter="creativeHoverOn"
           @mouseleave="creativeHoverOff"
         >{{ technologistOn ? technologistBinary[1] : technologistText[1] }}</span>
-        {{ technologistOn ? technologistBinary[2] : technologistText[2] }}
+        <span
+          class="about-technologist-text"
+        >{{ technologistOn ? technologistBinary[2] : technologistText[2] }}</span>
         <span
           class="technologist"
           @mouseenter="technologistHoverOn"
           @mouseleave="technologistHoverOff"
         >technologist</span>
-        {{ technologistOn ? technologistBinary[3] : technologistText[3] }}
+        <span
+          class="about-technologist-text"
+        >{{ technologistOn ? technologistBinary[3] : technologistText[3] }}</span>
         <br />
         <br />- Tanuj
       </p>
@@ -27,10 +33,13 @@
 </template>
 
 <script>
+import baffle from "baffle";
+
 export default {
   data() {
     return {
       creativeOn: false,
+      creativeDone: false,
       technologistOn: false,
       technologistBinary: [
         "0 100101100 000 0101001011 1101 01010 0 0000 00 001101011 0110100 1010 111 100 1000011 0110 110 001101101 0 0110 00 1 ",
@@ -43,7 +52,8 @@ export default {
         "creative",
         " ",
         " at the intersection of technology and design. Over the last few years, these fields have been increasingly integrated. I intend to use my unique skillset to continue pushing the boundaries of the way we build and interact with the connected world around us in new and meaningful ways."
-      ]
+      ],
+      baffleObject: null
     };
   },
   methods: {
@@ -52,18 +62,32 @@ export default {
     },
     creativeHoverOff: function() {
       this.creativeOn = false;
+      this.creativeDone = true;
+      setTimeout(() => {
+        this.creativeDone = false;
+      }, 700);
     },
     technologistHoverOn: function() {
       if (!this.creativeOn) this.technologistOn = !this.technologistOn;
+      this.baffleObject.start(700);
     },
     technologistHoverOff: function() {
       this.technologistOn = false;
+      this.baffleObject.reveal(500);
     }
+  },
+  mounted() {
+    this.baffleObject = baffle(".about-technologist-text", {
+      characters: "10",
+      speed: 200
+    });
   }
 };
 </script>
 
 <style lang="scss">
+@import "../styles/about-styles.scss";
+
 .about-text-wrapper {
   height: 100%;
   flex-grow: 1;
@@ -71,80 +95,20 @@ export default {
 
 .about-text {
   width: 60%;
-  // flex-grow: 1;
 
   font-size: 1.5rem;
   cursor: default;
-
-  .creative,
-  .technologist {
-    cursor: pointer;
-    text-decoration: underline solid;
-    font-weight: var(--font-weight-bold);
-    color: var(--primary-color);
-  }
 }
+
 .marquee-about {
   align-self: flex-start;
   user-select: none;
-}
 
-.marquee-text-wrap {
-  padding: 0.5em 0 0.5em 0;
-}
-
-.marquee-text-text {
-  font-size: 1em;
-}
-
-.about-text-creative {
-  user-select: none;
-
-  .creative {
-    transform: none;
-    font-family: var(--font-family);
-    font-weight: var(--font-weight-bold);
-  }
-
-  background: linear-gradient(
-    100deg,
-    #ff3b3b,
-    #fa9826,
-    #ffd400,
-    #c2ff00,
-    #00ffde,
-    #6ae2cb,
-    #00ccff,
-    #00ffde,
-    #c2ff00,
-    #ffd400,
-    #fa9826,
-    #ff3b3b
-  );
-  background-size: 200% auto;
-
-  background-clip: text;
-  color: transparent;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-
-  animation: rainbow 1.5s linear infinite;
-  @keyframes rainbow {
-    to {
-      background-position: 200% center;
+  .marquee-text-wrap {
+    padding: 0.5em 0 0.5em 0;
+    .marquee-text-text {
+      font-size: 1em;
     }
-  }
-}
-
-.about-text-technologist {
-  user-select: none;
-
-  color: green;
-  .creative,
-  .technologist {
-    text-decoration: none;
-    color: green;
-    font-weight: var(--font-weight-normal);
   }
 }
 </style>
