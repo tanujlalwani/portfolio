14900047484648cf86c856ce0048b7b81230dfb2
Tanuj <tanuj.lalwani@protonmail.com>
2019-08-31 18:03:09 -0700

    [REFACTOR]: Update PostCover to work with new Post component

--- src/components/PostCover.vue
+++ src/components/PostCover.vue
@@ -1,24 +1,22 @@
 <template>
   <div class="post-cover">
     <video
+      id="posts-view-video"
       v-if="post.coverVideo"
-      :poster="getCoverImage(post.path)"
+      :poster="getCoverImage"
       preload="none"
       playsinline
       muted
-      class="post-list-item-video size-fill-viewport"
-      :id="'post-list-item-video-' + post.id"
+      class="post-list-item-video"
+      autoplay
       loop
-      @mouseenter="playVideo(post.id)"
-      @mouseleave="pauseVideo(post.id)"
-    >
-      <source :src="getCoverVideo(post.path)" type="video/webm" />
-      <source :src="getCoverVideoAlt(post.path)" type="video/mp4" />
-    </video>
+      :src="getCoverVideo"
+      type="video/webm"
+    ></video>
     <img
       v-else
       class="post-list-item-image size-fill-viewport"
-      :src="getCoverImage(post.path)"
+      :src="getCoverImage"
       type="image/png"
     />
   </div>
@@ -28,22 +26,18 @@
 export default {
   name: "post-cover",
   props: ["post"],
-  methods: {
+  computed: {
+    path() {
+      return this.post.path;
+    },
     getCoverImage(path) {
-      return require("../assets/posts/" + path + "/cover.png");
+      return require("../assets/posts/" + this.path + "/cover.png");
     },
     getCoverVideo(path) {
-      return require("../assets/posts/" + path + "/cover.webm");
+      return require("../assets/posts/" + this.path + "/cover.webm");
     },
     getCoverVideoAlt(path) {
-      return require("../assets/posts/" + path + "/cover.mp4");
-    },
-    playVideo(id) {
-      document.getElementById("post-list-item-video-" + id).play();
-    },
-    pauseVideo(id) {
-      document.getElementById("post-list-item-video-" + id).pause();
-      document.getElementById("post-list-item-video-" + id).currentTime = 0;
+      return require("../assets/posts/" + this.path + "/cover.mp4");
     }
   }
 };
@@ -51,20 +45,15 @@ export default {
 
 <style lang="scss">
 .post-cover {
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
 
   .post-list-item-image,
   .post-list-item-video {
-    width: 85vw;
-    height: 90vh;
-    border-radius: 10px;
-    margin: 5vh auto;
+    width: 100%;
+    height: 100%;
     display: block;
     object-fit: cover;
-
-    // transition-delay: 2s;
-    // transition: all 1000ms ease-in;
   }
 
   video[poster] {
