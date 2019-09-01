740ed78734053fb4fed0ce5cfd6541ad340b8dc4
Tanuj <tanuj.lalwani@protonmail.com>
2019-09-01 05:03:09 -0700

    [STYLE]: Add media queries to Post component

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="post">
     <toc></toc>
     <post-cover class="post-header" id="cover" v-bind:post="post"></post-cover>
     <hr class="cover-divider" />
@@ -8,6 +8,8 @@
 </template>
 
 <script>
+import { EventBus } from "../event-bus.js";
+
 import TOC from "@components/TOC.vue";
 import PostCover from "@components/PostCover.vue";
 
@@ -30,6 +32,7 @@ export default {
   },
   mounted() {
     document.getElementById("cover").className += " full-cover";
+    EventBus.$emit("post-opened");
   }
 };
 </script>
@@ -77,6 +80,10 @@ export default {
 }
 
 /* Post Body - CSS Grid Layout */
+.post {
+  background-color: white;
+}
+
 .post-body {
   display: grid;
   grid-template-columns:
@@ -276,5 +283,239 @@ export default {
 
 .image-rounded img {
   border-radius: 0.5rem;
+}
+
+/* Media Queries */
+
+@media only screen and (min-width: 1000px) and (max-width: 1400px) {
+  .post-body {
+    grid-template-columns:
+      [page-start]
+      minmax(0px, 2.5fr)
+      [body-start]
+      minmax(0px, 1fr)
+      [header-start]
+      2.5fr
+      [header-end]
+      0.35fr
+      [text-start]
+      7fr
+      [text-end]
+      minmax(0px, 1fr)
+      [body-end]
+      minmax(0px, 2.5fr)
+      [page-end];
+  }
+}
+
+@media only screen and (min-width: 800px) and (max-width: 1000px) {
+  .post-body {
+    grid-template-columns:
+      [page-start]
+      minmax(0px, 1.5fr)
+      [body-start]
+      minmax(0px, 1fr)
+      [header-start]
+      3fr
+      [header-end]
+      0.35fr
+      [text-start]
+      7fr
+      [text-end]
+      minmax(0px, 1fr)
+      [body-end]
+      minmax(0px, 1.5fr)
+      [page-end];
+  }
+
+  .image-width-body {
+    grid-column: page-start/page-end;
+  }
+
+  .image-width-text {
+    grid-column: body-start/body-end;
+    margin-bottom: 0;
+  }
+}
+
+@media only screen and (min-width: 700px) and (max-width: 800px) {
+  .post-body {
+    grid-template-columns:
+      [page-start]
+      minmax(0px, 1fr)
+      [body-start]
+      minmax(0px, 1fr)
+      [header-start]
+      2.5fr
+      [header-end]
+      0.35fr
+      [text-start]
+      7fr
+      [text-end]
+      minmax(0px, 1fr)
+      [body-end]
+      minmax(0px, 1fr)
+      [page-end];
+  }
+
+  .image-width-body {
+    grid-column: page-start/page-end;
+
+    .image-width-body.image-rounded {
+      img {
+        border-radius: 0;
+      }
+    }
+  }
+
+  .image-width-text {
+    grid-column: body-start/body-end;
+    margin-bottom: 0;
+  }
+}
+
+@media only screen and (max-width: 700px) {
+  .post-body {
+    grid-template-columns:
+      [page-start]
+      2fr
+      [body-start]
+      8fr
+      [body-end]
+      2fr
+      [page-end];
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      grid-column: body-start/body-end;
+    }
+
+    h2#introduction {
+      grid-row: 1;
+    }
+
+    p {
+      grid-column: body-start/body-end;
+      padding-top: 1.5rem;
+    }
+
+    .image-caption {
+      grid-column: body-start/body-end;
+    }
+
+    ul {
+      grid-column: body-start/body-end;
+    }
+
+    .introduction {
+      grid-column: body-start/body-end;
+    }
+
+    hr {
+      grid-column: page-start/page-end;
+      margin: 3rem 0;
+    }
+
+    .image-width-text {
+      grid-column: page-start/page-end;
+      margin: 1rem auto;
+
+      .image-width-text.image-rounded {
+        img {
+          border-radius: 0;
+        }
+      }
+    }
+
+    .image-width-body {
+      grid-column: page-start/page-end;
+      margin: 2rem 0;
+
+      .image-width-body.image-rounded {
+        img {
+          border-radius: 0;
+        }
+      }
+    }
+
+    .image-width-full {
+      margin: 2rem auto 0 auto;
+    }
+
+    .image-inline {
+      margin: 1rem auto;
+    }
+
+    .image-group-horizontal {
+      p {
+        flex-flow: column wrap;
+
+        img {
+          width: 100%;
+        }
+      }
+    }
+
+    h1 {
+      font-size: 1.4rem;
+    }
+
+    h2,
+    h3,
+    h4,
+    p,
+    li {
+      font-size: 1.2rem;
+    }
+
+    ul {
+      margin-left: 3rem;
+    }
+
+    .introduction {
+      font-size: 1.2rem;
+    }
+  }
+}
+
+@media only screen and (max-width: 500px) {
+  .post-body {
+    grid-template-columns:
+      [page-start]
+      1fr
+      [body-start]
+      8fr
+      [body-end]
+      1fr
+      [page-end];
+
+    h1 {
+      font-size: 1.6rem;
+    }
+
+    h2,
+    h3,
+    h4,
+    p,
+    li {
+      font-size: 1.4rem;
+    }
+
+    ul {
+      margin-left: 2rem;
+    }
+
+    .introduction {
+      font-size: 1.4rem;
+    }
+
+    .post-footer {
+      margin: 4rem auto;
+    }
+  }
 }
 </style>
