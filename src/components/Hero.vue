16ded41a635df478d7153f1b2a89457352d8ddf6
Tanuj <tanuj.lalwani@protonmail.com>
2019-09-29 01:56:35 -0700

    [ADD]: Setup Tangerine's basic framework

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,27 +1,102 @@
 <template>
-  <section class="hero size-fill-viewport display-flex-column scroll-snap-start">
-    <div class="hero-text user-select-none">
-      They said give your website some personality.
-      <br class="line-break" />So he gave it
-      <span class="hero-text-italic">a</span> personality.
+  <section class="hero size-fill-viewport scroll-snap-start">
+    <div id="tangerine" class="hero-text user-select-none"></div>
+    <div class="instructions">
+      <span
+        class="instructions-text user-select-none"
+        v-show="instructionsText"
+        v-html="instructionsText"
+      ></span>
     </div>
+    <audio id="jazz" volume="0.1">
+      <source src="../assets/sounds/jazz.mp3" type="audio/mp3" />
+    </audio>
   </section>
 </template>
 
 <script>
-export default {};
+import TypeIt from "typeit";
+
+export default {
+  data() {
+    return {
+      instructionsText: false,
+      audioObj: null
+    };
+  },
+  mounted() {
+    this.audioObj = document.getElementById("jazz");
+    const instance = new TypeIt("#tangerine")
+      .options({
+        html: true,
+        speed: 0,
+        waitUntilVisible: true
+      })
+      .type(
+        `They said give your website some personality.
+          <br/>
+          So he gave it <i>a</i> personality. He made me.
+          <br/><br/>
+          Hi! I'm Tangerine. Nice to meet you :)`
+      )
+      .exec(async () => {
+        this.instructionsText = "Press any key to continue";
+        await new Promise((resolve, reject) => {
+          document.onkeypress = function(evt) {
+            evt = evt || window.event;
+            var charCode = evt.keyCode || evt.which;
+            var charStr = String.fromCharCode(charCode);
+            resolve();
+          };
+        });
+        this.instructionsText = null;
+      })
+      .empty()
+      .type(
+        `I wasn't expecting anyone to come around today!
+        <br/>I'll just get everything set up.
+        <br/>Make yourself comfortable, have a cookies or something?`
+      )
+      .type(
+        `How about some music?
+        <br/>People with a great taste in music are supposed to personable, right?`
+      )
+      .exec(async () => {
+        this.instructionsText = "Y/N";
+        await new Promise((resolve, reject) => {
+          document.onkeypress = function(evt) {
+            evt = evt || window.event;
+            var charCode = evt.keyCode || evt.which;
+            var charStr = String.fromCharCode(charCode);
+            resolve();
+          };
+        });
+        this.audioObj.play();
+      })
+      .go();
+  }
+};
 </script>
 
 <style lang="scss">
 .hero {
+  width: 100vw;
+  height: 100vh;
+
+  display: grid;
+  grid-template-rows: repeat(9, 1fr);
+
   background-color: var(--primary-color);
-  justify-content: flex-start;
 
   border: 0.3rem solid #fff;
 
   .hero-text {
     width: 80%;
-    margin: 3em;
+    margin: 0 10%;
+
+    height: 100%;
+
+    grid-row: 2/9;
 
     font-size: 2.1rem;
     font-weight: var(--font-weight-bold);
@@ -29,9 +104,35 @@ export default {};
 
     line-height: 1.3em;
 
-    .hero-text-italic {
+    i {
       font-weight: var(--font-weight-normal);
       font-style: italic;
+    }
+  }
+
+  .instructions {
+    height: 100%;
+    margin: 0 10%;
+
+    grid-row: 9/10;
+
+    .instructions-text {
+      background: #fff;
+      padding: 1rem 1.5rem;
+      border-radius: 0.5rem;
+      color: var(--primary-color);
+      font-weight: var(--font-weight-bold);
+      font-size: 1.4rem;
+      animation: fade 1s ease infinite alternate;
+    }
+  }
+
+  @keyframes fade {
+    from {
+      opacity: 0.5;
+    }
+    to {
+      opacity: 1;
     }
   }
 }
