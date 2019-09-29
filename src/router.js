33696fe70b24e398c661e5b62c221f52d83a2006
Tanuj <tanuj.lalwani@protonmail.com>
2019-09-29 15:25:20 -0700

    [WEBPACK]: Add ability to lazy load component with Vue Router

--- src/router.js
+++ src/router.js
@@ -1,11 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@views/Home.vue';
-import Post from '@views/Post.vue';
 
 import Posts from '@posts/posts.json';
 
 Vue.use(Router);
+
+function loadView(view) {
+  return () =>
+    import(/* webpackChunkName: "view-[request]" */ `@/views/${view}.vue`);
+}
 
 export default new Router({
   mode: 'history',
@@ -21,25 +24,16 @@ export default new Router({
       path: '/',
       name: 'home',
       meta: { index: 0 },
-      component: Home
+      component: loadView('Home')
     },
     {
       path: '/post/:title',
       name: 'post',
       meta: { index: 1 },
-      component: Post,
+      component: loadView('Post'),
       props: route => ({
         post: Posts.posts.filter(post => route.params.title == post.path)[0]
       })
     }
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () =>
-    //     import(/* webpackChunkName: "about" */ './views/About.vue')
-    // }
   ]
 });
